f6c86de074045ab53ded024eb195e51662484128
Ebube <nwokegodwin45@gmail.com>
2024-06-21 22:54:48 +0100

    trying to fix require not defined error in netlify build command changed

--- vite.config.js
+++ vite.config.js
@@ -2,22 +2,18 @@ import path from "path"
 // import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import commonjs from '@rollup/plugin-commonjs';
+import vitePluginRequire from "vite-plugin-require";
 
 // https://vitejs.dev/config/
 export default defineConfig({
- plugins: [commonjs()],
+ plugins: [commonjs(), vitePluginRequire.default(),],
  resolve: {
    alias: {
      "@": path.resolve(__dirname, "./src"),
    },
  },
  build: {
-    sourcemap: true, // Enables source maps
-    rollupOptions: {
-      commonjsOptions: {
-        transformMixedEsModules: true,
-      },
-    },
+  commonjsOptions: { transformMixedEsModules: true }
   },
 })
 
