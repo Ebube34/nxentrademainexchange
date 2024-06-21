b0307d767d040368df0aee2002ace12c17751d88
Ebube <nwokegodwin45@gmail.com>
2024-06-21 22:14:20 +0100

    trying to fix require not defined error in netlify added @rollup/plugin-commonjs module to vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import path from "path"
-import react from "@vitejs/plugin-react"
+// import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import commonjs from '@rollup/plugin-commonjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
