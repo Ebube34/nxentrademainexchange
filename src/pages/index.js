bb4b68d074ffbd631ac082dc7115fcc4dd28a2d7
Ebube <nwokegodwin45@gmail.com>
2024-06-22 15:02:25 +0100

    latest commit no errors last time no more errors

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,13 @@
 import Home from "./home"
 import SignUp from "./sign up"
 import VerifyEmail from "./verify email"
-<<<<<<< HEAD
-import EmailLogic from "./email logic"
-import SignIn from "./sign in"
-=======
 import SignIn from "./sign in"
 import EmailLogic from "./email logic"
->>>>>>> 0619da6 (autheticating redone and completed)
 
 export {
    Home,
    SignUp,
    VerifyEmail,
-<<<<<<< HEAD
-   EmailLogic,
-   SignIn
-=======
    SignIn,
    EmailLogic
->>>>>>> 0619da6 (autheticating redone and completed)
 }
